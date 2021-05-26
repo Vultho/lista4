a223b4f1e4ce45673919bfeb045e3a0a4c985a83
Vultho <82899577+Vultho@users.noreply.github.com>
2021-05-26 16:58:28 -0300

    lista4

--- src/com/company/exercicio4.java
+++ src/com/company/exercicio4.java
@@ -1,16 +1,26 @@
-package com.company;
+package company;
 
 import java.util.Scanner;
 
 public class exercicio4 {
-    public static void main(String[] args) {
-        char letras[] = new char[10];
-        char vogais[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        for (int i = 0; i < letras.length; i++) {
+
+    public static void main(String[]args){
+
+        char[] vet = new char[10];
+        int count = 0;
+
+        for (int i=0; i < vet.length; i++){
             System.out.println("Digite as letras");
-            Scanner le = new Scanner(System.in);
-            letras[i] = le.next().charAt(0);
-            // terminar...
+            Scanner letras= new Scanner(System.in);
+            vet[i]= letras.next().charAt(0);
+            if((vet[i] !='a') && (vet[i] != 'e') && (vet[i] != 'i') &&
+                    (vet[i] != 'o') && (vet[i] != 'u')){
+                count = count + 1;
+                System.out.println(vet[i]);
+
+            }
         }
+        System.out.println("numero de consoante  ");
+        System.out.print(count);
     }
 }
--- src/com/company/exercicio5.java
+++ src/com/company/exercicio5.java
@@ -1,18 +1,8 @@
-package com.company;
-
-import java.util.Scanner;
+package company;
 
 public class exercicio5 {
     public static void main(String[] args) {
         int numeros[] = new int[20];
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("valores posto no vetor");
-            Scanner num = new Scanner(System.in);
-            numeros[i] = num.nextInt();
-
-
-        }
-
-
+        // continuar
     }
 }
