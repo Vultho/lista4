3b7ece06c1df4c00db64e49699a009d4ea93bf1d
Vultho <82899577+Vultho@users.noreply.github.com>
2021-06-08 17:04:01 -0300

    a

--- src/com/company/exercicio10.java
+++ src/com/company/exercicio10.java
@@ -2,6 +2,10 @@ package company;
 
 public class exercicio10 {
     public static void main(String[] args) {
-    
+        int[] vetor1=new int[10];
+        int[] vetor2= new int[10];
+        int[] vetorJunto= new int [20];
+        
+
     }
 }
